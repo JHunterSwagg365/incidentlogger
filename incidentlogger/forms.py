09b83c0b8922a754d3f832d37fa778507b8373eb
JHunterSwagg365 <jhunterswagg365@gmail.com>
2018-12-07 14:43:01 -0500

    Adds signin authentication as well as usage of the database to store users and putting some actions behind a login wall

--- incidentlogger/forms.py
+++ incidentlogger/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, DateField, TextAreaField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from incidentlogger.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -12,6 +13,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
         validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username not available, Please pick a different one.')
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('An account has already been maid with that email. Please pick a different one or Login.')
 
 
 class LoginForm(FlaskForm):
